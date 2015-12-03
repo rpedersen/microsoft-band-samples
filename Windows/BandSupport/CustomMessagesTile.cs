8758287cfba174fc3cf5b8151ea1a288fbd1ada0
Ryan Pedersen <ryan@pedersens.us>
2015-12-02 17:26:41 -0800

    updating Custom Messages Tile

--- Windows/BandSupport/CustomMessagesTile.cs
+++ Windows/BandSupport/CustomMessagesTile.cs
@@ -8,6 +8,9 @@ namespace BandSupport
         {
             Id = new Guid("EC6941B0-4793-4915-9A4C-C2647E94FBE9");
             Name = "Custom Messages";
+            Kind = AppBandTileKind.CustomMessages;
+            TileIconUri = new Uri("ms-appx:///Assets/CustomMessagesTileLarge.png");
+            SmallIconUri = new Uri("ms-appx:///Assets/CustomMessagesTileSmall.png");
         }
     }
 }
